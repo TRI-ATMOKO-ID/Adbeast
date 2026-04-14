f6d4d2e2e15c70516af00b2f6c330336e2c1c45b
TRI ATMOKO ID <72822376+TRI-ATMOKO-ID@users.noreply.github.com>
2026-04-15 02:50:44 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ clear
 echo "[*] Memulai Instalasi AdBeast Bot..."
 
 # Download file biner langsung dari GitHub dan taruh di folder sistem Termux
-wget -qO $PREFIX/bin/adbeast https://raw.githubusercontent.com/UsernameKamu/RepoKamu/main/botku.bin
+wget -qO $PREFIX/bin/adbeast https://github.com/TRI-ATMOKO-ID/Adbeast/blob/main/botku.bin
 
 # Berikan izin eksekusi
 chmod +x $PREFIX/bin/adbeast
